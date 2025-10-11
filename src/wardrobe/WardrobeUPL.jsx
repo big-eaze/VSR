e6f6edfe0b4b1e2e62e9f483a4c6b01b3aea82d3
Israel <israelojeleye35@gmail.com>
2025-10-11 07:41:21 +0100

    delete not-in-use images

--- src/wardrobe/WardrobeUPL.jsx
+++ src/wardrobe/WardrobeUPL.jsx
@@ -239,6 +239,7 @@ export default function WardrobeUploadPage() {
                 </button>
                 {/* Action button */}
                 {preview && (
+
                   <button
                     onClick={saveToWardrobe}
                     disabled={!isFormComplete}
